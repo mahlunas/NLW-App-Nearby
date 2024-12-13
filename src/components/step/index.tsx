d60ec38af0ae1ef6dc143d5d02ad9b32a7dd1335
Luna <lunasouzamar@gmail.com>
2024-12-13 13:58:07 -0300

    att: tela inicial

---
+++ src/components/step/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+import { s } from "./styles";
+import { IconProps } from "@tabler/icons-react-native";
+import { colors } from "@/styles/colors";
+
+type Props = {
+    title: string
+    description: string
+    icon: React.ComponentType<IconProps>
+}
+
+export function Step({title, description, icon: Icon} : Props){
+    return (
+        <View style={s.container}>
+            <Icon size={32} color={colors.red.base}></Icon>
+            <View style={s.details}>
+                <Text style={s.title}>{title}</Text>
+                <Text style={s.description}>{description}</Text>
+            </View>
+        </View>
+    )
+}
